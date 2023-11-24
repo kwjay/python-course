007ef853c066d60b09d90ddf4d22d086a7790e24
kwjay <kwjay1002@gmail.com>
2023-11-24 23:40:11 +0100

    checking conditions for sending email and sending it

--- iss-over-head/main.py
+++ iss-over-head/main.py
@@ -1,8 +1,21 @@
 import requests
 from datetime import datetime
+import smtplib
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_EMAIL = "mail@gmail.com"
+MY_PASSWORD = "password"
+MY_PROVIDER = "smtp.gmail.com"
+
+
+def send_email():
+    with smtplib.SMTP(MY_PROVIDER) as server:
+        server.starttls()
+        server.login(user=MY_EMAIL, password=MY_PASSWORD)
+        server.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg="Subject:LOOK UP\n\nISS IS OVER HEAD!!!")
+
+
+MY_LAT = 51.507351
+MY_LONG = -0.127758
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -10,9 +23,6 @@ data = response.json()
 
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
-
-#Your position is within +5 or -5 degrees of the ISS position.
-
 
 parameters = {
     "lat": MY_LAT,
@@ -25,10 +35,15 @@ response.raise_for_status()
 data = response.json()
 sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
+print(sunset)
+print(sunrise)
 time_now = datetime.now()
+hour = time_now.hour
 
-#If the ISS is close to my current position
+if MY_LONG - 5 <= iss_longitude <= MY_LONG + 5 and MY_LAT - 5 <= iss_latitude <= MY_LAT + 5:
+    if sunset < hour or hour < sunrise:
+        send_email()
+
 # and it is currently dark
 # Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
