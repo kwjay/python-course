f0242863431646541e366369d9bcc9c7aa9b97b1
kwjay <kwjay1002@gmail.com>
2023-11-27 23:41:55 +0100

    finally got my api key :)

--- weather-forecast/main.py
+++ weather-forecast/main.py
@@ -4,8 +4,14 @@ params = {
     "lat": 52.406376,
     "lon": 16.925167,
     "appid": "",
+    "cnt": 4,
 }
 response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params)
-print(response.status_code)
 response.raise_for_status()
-print(response.json())
+weather_data = response.json()
+print(weather_data)
+for timestamp in weather_data["list"][0]["weather"]:
+    if timestamp["id"] < 700:
+        print("Bring an umbrella")
+    else:
+        print("No need to worry about getting wet")
