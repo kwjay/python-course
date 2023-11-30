dac580643e779a87f6a771f0ddc065b8dd092d31
kwjay <kwjay1002@gmail.com>
2023-11-30 20:24:27 +0100

    creating graph

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,13 +1,34 @@
 import requests
 
-PIXELA_URL = "https://pixe.la/v1/users"
+PIXELA_URL = "https://pixe.la/v1"
+USERNAME = ""
+TOKEN = ""
 
+USERS_ENDPOINT = PIXELA_URL + "/users"
 USER_PARAMS = {
-    "token": "",
-    "username": "",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
 # FIRST TIME ACCOUNT CREATION
-# response = requests.post(url=PIXELA_URL, json=USER_PARAMS)
+# response = requests.post(url=USERS_ENDPOINT, json=USER_PARAMS)
+# print(response.text)
+
+# CREATING GRAPH
+GRAPH_ENDPOINT = PIXELA_URL + f"/users/{USERNAME}/graphs"
+GRAPH_CONFIG = {
+    "id": "graph1",
+    "name": "Workout Graph",
+    "unit": "sessions",
+    "type": "int",
+    "color": "kuro",
+}
+
+HEADERS = {
+    "X-USER-TOKEN": TOKEN
+}
+# response = requests.post(url=GRAPH_ENDPOINT, json=GRAPH_CONFIG, headers=HEADERS)
+# print(response.text)
+
