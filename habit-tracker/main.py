3b8cb15ef7c3b713850ae482b3b3fc9fbfc9e770
kwjay <kwjay1002@gmail.com>
2024-01-02 18:12:15 +0100

    finished habit tracker

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 PIXELA_URL = "https://pixe.la/v1"
 USERNAME = ""
@@ -14,14 +15,13 @@ USER_PARAMS = {
 
 # FIRST TIME ACCOUNT CREATION
 # response = requests.post(url=USERS_ENDPOINT, json=USER_PARAMS)
-# print(response.text)
 
 # CREATING GRAPH
 GRAPH_ENDPOINT = PIXELA_URL + f"/users/{USERNAME}/graphs"
 GRAPH_CONFIG = {
-    "id": "graph1",
-    "name": "Workout Graph",
-    "unit": "sessions",
+    "id": "ctci",
+    "name": "Cracking the coding interview",
+    "unit": "pages",
     "type": "int",
     "color": "kuro",
 }
@@ -30,5 +30,19 @@ HEADERS = {
     "X-USER-TOKEN": TOKEN
 }
 # response = requests.post(url=GRAPH_ENDPOINT, json=GRAPH_CONFIG, headers=HEADERS)
-# print(response.text)
 
+PIXEL_CREATION_ENDPOINT = GRAPH_ENDPOINT + "/" + GRAPH_CONFIG["id"]
+today = datetime.now()
+PIXEL_CREATION_CONFIG = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "2",
+}
+# response = requests.post(url=PIXEL_CREATION_ENDPOINT, json=PIXEL_CREATION_CONFIG, headers=HEADERS)
+
+pixel_date = datetime(year=2024, month=1, day=2).strftime("%Y%m%d")
+PIXEL_UPDATE_ENDPOINT = PIXEL_CREATION_ENDPOINT + "/" + pixel_date
+PIXEL_UPDATE_CONFIG = {
+    "quantity": "4",
+}
+response = requests.put(url=PIXEL_UPDATE_ENDPOINT, json=PIXEL_UPDATE_CONFIG, headers=HEADERS)
+print(response.text)
